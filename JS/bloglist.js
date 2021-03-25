73b908aaaefddc2ee804918b4b6e24616df97f8f
kapilpiyush2409 <kapilpiyush33@gmail.com>
2021-03-25 19:31:18 +0530

    upgrad project

---
+++ JS/bloglist.js
@@ -0,0 +1,99 @@
+var modal = document.getElementById("myModal");
+
+
+var btn = document.getElementById("si");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modal1 = document.getElementById("myModal1");
+
+
+var btn1 = document.getElementById("su");
+
+
+var span1 = document.getElementsByClassName("close1")[0];
+
+
+btn1.onclick = function() {
+    modal1.style.display = "block";
+}
+
+
+span1.onclick = function() {
+    modal1.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
+
+var sp = document.getElementById("redirect");
+sp.onclick = function() {
+    modal.style.display = "none";
+    modal1.style.display = "block";
+}
+
+var modal3 = document.getElementById("myModal3");
+
+var btn31 = document.getElementsByClassName("abc1");
+
+var span3 = document.getElementById("na");
+
+
+btn31[0].onclick = function() {
+    modal3.style.display = "block";
+}
+btn31[1].onclick = function() {
+    modal3.style.display = "block";
+}
+btn31[2].onclick = function() {
+    modal3.style.display = "block";
+}
+btn31[3].onclick = function() {
+    modal3.style.display = "block";
+}
+btn31[4].onclick = function() {
+    modal3.style.display = "block";
+}
+
+span3.onclick = function() {
+    modal3.style.display = "none";
+}
+
+var btn32 = document.getElementsByClassName("fa");
+btn32[0].onclick = function() {
+    window.location.href = 'post.html';
+}
+btn32[1].onclick = function() {
+    window.location.href = 'post.html';
+}
+btn32[2].onclick = function() {
+    window.location.href = 'post.html';
+}
+btn32[3].onclick = function() {
+    window.location.href = 'post.html';
+}
+btn32[4].onclick = function() {
+    window.location.href = 'post.html';
+}
---
+++ JS/index.js
@@ -0,0 +1,72 @@
+var modal = document.getElementById("myModal");
+
+
+var btn = document.getElementById("si");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modal1 = document.getElementById("myModal1");
+
+
+var btn1 = document.getElementById("su");
+
+
+var span1 = document.getElementsByClassName("close1")[0];
+
+
+btn1.onclick = function() {
+    modal1.style.display = "block";
+}
+
+
+span1.onclick = function() {
+    modal1.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
+
+var sp = document.getElementById("redirect");
+sp.onclick = function() {
+    modal.style.display = "none";
+    modal1.style.display = "block";
+}
+
+var modal2 = document.getElementById("myModal2");
+
+
+var btn2 = document.getElementById("createp");
+
+
+var span2 = document.getElementsByClassName("close2")[0];
+
+
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+
+
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
